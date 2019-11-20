ed942889c439ac9cfca9cbeb8d0dc122fc42103d
Timothy K. Serem <kipkorirtimserem@outlook.com>
2019-11-20 21:51:29 +0300

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  cart = [];
+  items = [];
 
+  sliderConfig = {
+    slidesPerView: 1.6,
+    spaceBetween: 10,
+    centeredSlides: true
+  };
+
+  constructor(private router: Router, private cartService: CartService) { }
+
+  ngOnInit() {
+    this.items = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+  }
+
+  addToCart(product) {
+    this.cartService.addProduct(product);
+  }
+
+  openCart() {
+    this.router.navigate(['cart']);
+  }
 }
